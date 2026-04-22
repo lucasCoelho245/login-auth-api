05b9df459e270cbdcc9a8adc085a4eff2efa56b1
Lucas Coelho <49642902+lucasCoelho245@users.noreply.github.com>
2026-04-22 11:36:58 -0300

    validação e criaçao de login

---
+++ src/main/java/login_auth_api/dto/LoginRequestDto.java
@@ -0,0 +1,5 @@
+package login_auth_api.dto;
+
+public record LoginRequestDto(String email, String password){
+
+}
---
+++ src/main/java/login_auth_api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package login_auth_api.repositories;
+
+import login_auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
